42ad8947ec5155c578a7df64fe334fe242b7ca48
Tibit <rktibit@gmail.com>
2025-03-10 20:52:21 +0530

    JFrame created

---
+++ src/main/java/com/mycompany/swingcalculator/SwingCalculator.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.swingcalculator;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Tibit
+ */
+public class SwingCalculator {
+
+    public SwingCalculator() {
+        JFrame jf = new JFrame("Calculator");
+        jf.setLayout(null);
+        jf.setSize(600,600);
+        jf.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        
+        SwingCalculator c = new SwingCalculator();
+
+    }
+}
